7951679c6095b5d4232c5aa690b9966884536cf0
Karol Wojtaszek <karol@appunite.com>
2012-11-09 21:05:27 +0100

    made it arced, added .gitignore, added library

---
+++ WEPopoverLib/WEPopoverLib.h
@@ -0,0 +1,13 @@
+//
+//  WEPopoverLib.h
+//  WEPopoverLib
+//
+//  Created by Karol Wojtaszek on 09.11.2012.
+//  Copyright (c) 2012 Werner IT Consultancy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WEPopoverLib : NSObject
+
+@end
---
+++ WEPopoverLib/WEPopoverLib.m
@@ -0,0 +1,13 @@
+//
+//  WEPopoverLib.m
+//  WEPopoverLib
+//
+//  Created by Karol Wojtaszek on 09.11.2012.
+//  Copyright (c) 2012 Werner IT Consultancy. All rights reserved.
+//
+
+#import "WEPopoverLib.h"
+
+@implementation WEPopoverLib
+
+@end
